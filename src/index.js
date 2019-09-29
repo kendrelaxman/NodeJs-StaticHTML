10970562bfc82ab09f9ec7a37026ba49836532ba
Laxmikant Kendre <kendrelaxman@users.noreply.github.com>
2019-09-28 21:49:28 -0500

    refactoring html

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-var http = require("http"),
-  fs = require("fs");
+// HTTP Module for Creating Server and Serving Static Files Using Node.js
+// Static Files: HTML, CSS, JS, Images
+// Get Complete Source Code from Pabbly.com
 
-// configure our application
-const Express = require("express");
-const app = new Express();
-app.use(Express.static(__dirname + "/assets"));
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
 
-fs.readFile("./index.html", function(err, html) {
-  if (err) {
-    throw err;
-  }
-  http
-    .createServer(function(request, response) {
-      response.writeHeader(200, { "Content-Type": "text/html" });
-      response.write(html);
-      response.end();
-    })
-    .listen(3000);
-});
+http
+  .createServer(function(req, res) {
+    if (req.url === "/") {
+      fs.readFile("./index.html", "UTF-8", function(err, html) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(html);
+      });
+    } else if (req.url.match(".css$")) {
+      var cssPath = path.join(__dirname, "public", req.url);
+      var fileStream = fs.createReadStream(cssPath, "UTF-8");
+      res.writeHead(200, { "Content-Type": "text/css" });
+      fileStream.pipe(res);
+    } else if (req.url.match(".img$")) {
+      var imagePath = path.join(__dirname, "public/img", req.url);
+      var fileStream = fs.createReadStream(imagePath);
+      res.writeHead(200, { "Content-Type": "image/jpg" });
+      fileStream.pipe(res);
+    } else {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("No Page Found");
+    }
+  })
+  .listen(3000);
